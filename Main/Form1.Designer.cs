f3cf3f87c15338fa101f972a3d3248d9492ca55b
Иван Хорошевский <butoxors@gmail.com>
2019-07-09 17:00:56 +0300

    Добавьте файлы проекта.

---
+++ Main/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Main
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.btnCalc = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(663, 5);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(125, 32);
+            this.btnCheck.TabIndex = 0;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(645, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 38);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(508, 309);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView2.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCellsExceptHeaders;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(526, 112);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dataGridView2.Size = new System.Drawing.Size(439, 235);
+            this.dataGridView2.TabIndex = 3;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(663, 43);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(125, 32);
+            this.btnCalc.TabIndex = 4;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(977, 359);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnCheck);
+            this.Name = "Form1";
+            this.Text = "SteamTradeChecker";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.TextBox textBox1;
+        public System.Windows.Forms.DataGridView dataGridView2;
+        public System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnCalc;
+    }
+}
+
---
+++ Main/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Main
+{
+    public partial class Form1 : Form
+    {
+        public SwapItems swapItems = null;
+        List<LootItems> lootItems = new List<LootItems>();
+        readonly HttpClient client = new HttpClient();
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private async void btnCheck_Click(object sender, EventArgs e)
+        {
+            await Task.Run(() => GetXHR());
+            GetLootItems("https://loot.farm/fullpriceDOTA.json");
+            dataGridView1.AutoGenerateColumns = true;
+            dataGridView1.DataSource = RowGenerator();
+        }
+        private DataTable RowGenerator()
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.AddRange(new DataColumn[]
+            {
+                new DataColumn("Name", typeof(string)),
+                new DataColumn("Swap", typeof(double)),
+                new DataColumn("Have(Swap)", typeof(int)),
+                new DataColumn("Max(Swap)", typeof(int)),
+                new DataColumn("Loot", typeof(double)),
+                new DataColumn("Have(Loot)", typeof(int)),
+                new DataColumn("Max(Loot)", typeof(int)),
+            });
+            var l = swapItems.Result.Where(x => x.Price.Value > 0).OrderByDescending(x => x.Price.Value);
+            foreach (var i in l)
+            {
+                var loot = lootItems.Where(x => x.Name == i.MarketName).ToList();
+                if (loot.Count != 0 && i.Stock.Have < i.Stock.Max && loot[0].Have < loot[0].Max && i.Stock.Have != 0 && loot[0].Have != 0)
+                {
+                    var row = dt.NewRow();
+                    row["Name"] = i.MarketName;
+                    row["Swap"] = Math.Round(i.Price.Value * 0.01, 2);
+                    row["Loot"] = Math.Round(loot[0].Price * 0.01, 2);
+                    row["Have(Swap)"] = i.Stock.Have;
+                    row["Max(Swap)"] = i.Stock.Have;
+                    row["Have(Loot)"] = loot[0].Have;
+                    row["Max(Loot)"] = loot[0].Max;
+                    dt.Rows.Add(row);
+                }
+            }
+            return dt;
+        }
+        private void GetLootItems(string URL)
+        {
+            try
+            {
+                using (var webClient = new System.Net.WebClient())
+                {
+                    var json = webClient.DownloadString(URL);
+
+                    lootItems = LootItems.FromJson(json);
+                }
+            }
+            catch (Exception) { }
+        }
+
+        private async Task GetXHR()
+        {
+            string responseBody = "";
+            
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync("https://api.swap.gg/prices/570");
+                response.EnsureSuccessStatusCode();
+                responseBody = await response.Content.ReadAsStringAsync();
+
+                swapItems = SwapItems.FromJson(responseBody);                
+            }
+            catch (HttpRequestException) { }
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataTable dt = new DataTable();
+
+                dt.Columns.AddRange(new DataColumn[]
+                {
+                    new DataColumn("Name", typeof(string)),
+                    new DataColumn("Swap", typeof(double)),
+                    new DataColumn("Loot", typeof(double)),
+                    new DataColumn("%", typeof(double))
+                });
+
+                for (int i = 0; i < dataGridView1.RowCount; i++)
+                {
+                    var x = Convert.ToDouble(dataGridView1.Rows[i].Cells[1].Value);
+                    var y = Convert.ToDouble(dataGridView1.Rows[i].Cells[4].Value);
+                    var have1 = Convert.ToInt32(dataGridView1.Rows[i].Cells[3].Value);
+                    var max1 = Convert.ToInt32(dataGridView1.Rows[i].Cells[4].Value);
+                    var have2 = Convert.ToInt32(dataGridView1.Rows[i].Cells[5].Value);
+                    var max2 = Convert.ToInt32(dataGridView1.Rows[i].Cells[6].Value);
+
+                    if (have1 > 0 || have1 < max1 && have2 > 0 && have2 < max2)
+                    {
+                        var row = dt.NewRow();
+
+                        row["Name"] = dataGridView1.Rows[i].Cells[0].Value.ToString();
+                        row["Swap"] = Math.Round(x, 2);
+                        row["Loot"] = Math.Round(y, 2);
+                        row["%"] = 100 - Math.Round((x / y) * 100, 2);
+
+                        dt.Rows.Add(row);
+                    }
+                }
+
+                dataGridView2.DataSource = dt;
+                dataGridView2.Update();
+            }
+            catch (Exception) { }
+        }
+    }
+}
