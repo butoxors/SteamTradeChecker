2b935f5ddd67dce41c9778aca63a968d688703f5
Иван Хорошевский <butoxors@gmail.com>
2019-07-09 18:11:07 +0300

    На пути к добавлению CSDEALS и DOTA.MONEY

--- Main/Form1.cs
+++ Main/Form1.cs
@@ -14,6 +14,7 @@ namespace Main
         public SwapItems swapItems = null;
         List<LootItems> lootItems = new List<LootItems>();
         readonly HttpClient client = new HttpClient();
+        DealsItems Deals = new DealsItems();
 
         public Form1()
         {
@@ -21,9 +22,14 @@ namespace Main
             
         }
 
-        private async void btnCheck_Click(object sender, EventArgs e)
+        private void btnCheck_Click(object sender, EventArgs e)
         {
-            await Task.Run(() => GetXHR());
+            var res = Task.Run(() => GetXHR("https://api.swap.gg/prices/570"));
+            swapItems = SwapItems.FromJson(res.Result);
+            //var res2 = Task.Run(() => GetXHR("https://dota.money/570/load_bots_inventory"));
+            //textBox1.Text = res2.Result;
+            //Task.Run(() => GetXHRDeals());
+
             GetLootItems("https://loot.farm/fullpriceDOTA.json");
             dataGridView1.AutoGenerateColumns = true;
             dataGridView1.DataSource = RowGenerator();
@@ -35,6 +41,7 @@ namespace Main
             {
                 new DataColumn("Name", typeof(string)),
                 new DataColumn("Swap", typeof(double)),
+                new DataColumn("Deals", typeof(double)),
                 new DataColumn("Have(Swap)", typeof(int)),
                 new DataColumn("Max(Swap)", typeof(int)),
                 new DataColumn("Loot", typeof(double)),
@@ -45,12 +52,15 @@ namespace Main
             foreach (var i in l)
             {
                 var loot = lootItems.Where(x => x.Name == i.MarketName).ToList();
+                //var deals = Deals.Response.Items.The570.Where(x => x.C == i.MarketName).ToList();
+
                 if (loot.Count != 0 && i.Stock.Have < i.Stock.Max && loot[0].Have < loot[0].Max && i.Stock.Have != 0 && loot[0].Have != 0)
                 {
                     var row = dt.NewRow();
                     row["Name"] = i.MarketName;
                     row["Swap"] = Math.Round(i.Price.Value * 0.01, 2);
                     row["Loot"] = Math.Round(loot[0].Price * 0.01, 2);
+                    //row["Deals"] = Math.Round(deals[0].I * 0.01, 2);
                     row["Have(Swap)"] = i.Stock.Have;
                     row["Max(Swap)"] = i.Stock.Have;
                     row["Have(Loot)"] = loot[0].Have;
@@ -74,19 +84,36 @@ namespace Main
             catch (Exception) { }
         }
 
-        private async Task GetXHR()
+        private async Task<string> GetXHR(string URL)
         {
             string responseBody = "";
             
             try
             {
-                HttpResponseMessage response = await client.GetAsync("https://api.swap.gg/prices/570");
+                HttpResponseMessage response = await client.GetAsync(URL);
                 response.EnsureSuccessStatusCode();
                 responseBody = await response.Content.ReadAsStringAsync();
-
-                swapItems = SwapItems.FromJson(responseBody);                
+                return responseBody;
             }
             catch (HttpRequestException) { }
+            return string.Empty;
+        }
+        private async Task GetXHRDeals()
+        {
+            var values = new Dictionary<string, string>
+            {
+               { "appid", "570" }
+            };
+
+            var content = new FormUrlEncodedContent(values);
+            client.DefaultRequestHeaders.Add("content-type", "application /json");
+            client.DefaultRequestHeaders.Add("accept", "application/json, text/javascript, */*; q=0.01");
+            client.DefaultRequestHeaders.Add("cookie", "__cfduid=d2f9f21cb8db798b61850080116104cad1552734072; _ga=GA1.2.703749747.1552734081; sessionID=sg6vmoq81bat5t1q9kpm3dqjgs; lang=ru; _gid=GA1.2.188995789.1562681437; hideIntro=1; sessionID=sg6vmoq81bat5t1q9kpm3dqjgs");
+            var response = await client.PostAsync("https://cs.deals/ajax/botsinventory", content);
+
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            Deals = DealsItems.FromJson(responseString);
         }
 
         private void btnCalc_Click(object sender, EventArgs e)
